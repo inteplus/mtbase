a2c0203332d2c2ed4357ccae605fdf9639aceab0
Minh-Tri Pham <pmtri80@gmail.com>
2020-08-27 11:10:01 +0100

    changed dask[distributed] to distributed

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='mtbase',
           'colorama',  # for colored text
           'Cython',  # for fast speed on tiny objects
           'dask', # for simple multiprocessing jobs
-          'dask[distributed]',  # for simple multiprocessing jobs
+          'distributed',  # for simple multiprocessing jobs
       ],
       url='https://github.com/inteplus/mtbase',
       project_urls={
