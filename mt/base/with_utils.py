3e60412a2f16e7bba17e683784991be14178867d
Minh-Tri Pham <pmtri80@gmail.com>
2021-02-06 18:21:36 +0000

    v1.1.3: better join_scopes

--- mt/base/with_utils.py
+++ mt/base/with_utils.py
@@ -38,15 +38,26 @@ class join_scopes(object):
     with join_scopes(a,b,c):
         statements
     ```
+
+    All positional arguments passed to the function are treated as a separate object to be used with the 'with' statement. The return value of the `__enter__`  method of the function is the list of return values of each argument's `__enter__` method.
     '''
 
     def __init__(self, *with_objs): 
-        self.with_objs = with_objs 
+        self.with_objs = with_objs
 
-    def __enter__(self): 
-        for with_obj in self.with_objs: 
-            with_obj.__enter__() 
+    def __enter__(self):
+        return [with_obj.__enter__() for with_obj in self.with_objs]
 
-    def __exit__(self, type, value, traceback): 
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type is None: # normal case
+            for with_obj in reversed(self.with_objs): 
+                with_obj.__exit__(exc_type, exc_value, traceback)
+            return
+
+        reraise = True
         for with_obj in reversed(self.with_objs): 
-            with_obj.__exit__(type, value, traceback) 
+            if with_obj.__exit__(exc_type, exc_value, traceback):
+                reraise = False
+                exc_type, exc_value, traceback = None, None, None
+
+        return None if reraise else True
