d2cd063d93aeb6eb539bade1c5bf5728560310bf
Minh-Tri Pham <pmtri80@gmail.com>
2021-02-07 16:44:56 +0000

    v1.1.7: clean up debug_exec() a bit

--- mt/base/exec.py
+++ mt/base/exec.py
@@ -1,10 +1,11 @@
 '''Exec utils.'''
 
 
+import threading as _t
 from .logging import logger
 
 
-__all__ = ['debug_exec', 'debug_exec2']
+__all__ = ['debug_exec']
 
 
 def debug_exec(func, *args, **kwargs):
@@ -35,6 +36,9 @@ def debug_exec(func, *args, **kwargs):
             return (user_data['func'])(*(user_data['args']), **(user_data['kwargs']))
             break
         except:
+            if _t.current_thread().__class__.__name__ != '_MainThread':
+                raise #  we only deal with main thread
+
             logger.warn_last_exception()
             try:
                 from IPython.terminal.embed import InteractiveShellEmbed
@@ -51,39 +55,3 @@ def debug_exec(func, *args, **kwargs):
                 if len(cmd) == 0:
                     raise
                 exec(cmd)
-
-def debug_exec2(func, *args, **kwargs):
-    '''Executes a function with trials.
-
-    This function executes a function. However, when an exception is raised in the function, it asks the user if they want reload a module and try again, before actually passing the exception up in the call stack.
-
-    Parameters
-    ----------
-    func : function
-        function to be executed
-    args : list
-        positional arguments of the function
-    kwargs : dict
-        keyword arguments of the function
-
-    Returns
-    -------
-    whatever the function returns
-    '''
-    while True:
-        try:
-            return func(*args, **kwargs)
-            break
-        except:
-            logger.warn_last_exception()
-            while True:
-                module_name = input("Enter a module to be reloaded and retry, or press ENTER to continue: ")
-                if len(module_name) == 0:
-                    raise
-                from sys import modules
-                if module_name not in modules:
-                    logger.warn("Module '{}' not yet loaded. Try again.".format(module_name))
-                    continue
-                import importlib
-                importlib.reload(modules[module_name])
-                break
