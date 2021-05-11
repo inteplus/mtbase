fed8684242695563f7cb9e4692dd269eb4bd6941
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2021-05-11 10:08:03 +0100

    v1.4.3: np.powm() introduced

--- mt/np/matrix.py
+++ mt/np/matrix.py
@@ -6,7 +6,7 @@
 import numpy as np
 
 
-__all__ = ['psd_approx', 'sqrtm']
+__all__ = ['psd_approx', 'sqrtm', 'powm']
 
 
 def psd_approx(A):
@@ -47,5 +47,25 @@ def sqrtm(A):
         output square matrix such that its square equals the input matrix
     '''
     W, V = np.linalg.eig(A) # A = V diag(W) V^{-1}
-    W = np.array([np.sqrt(x) if x >= 0 else 0 for x in W])
+    W = np.array([np.sqrt(x) if x > 0 else 0 for x in W])
+    return V @ np.diag(W) @ V.T
+
+
+def powm(A, exp):
+    '''Computes the matrix power of a real positive semi-definite matrix.
+
+    Parameters
+    ----------
+    A : numpy.ndarray
+        2D array representing the input matrix
+    exp : float
+        power exponent
+
+    Returns
+    -------
+    numpy.ndarray
+        output square matrix such that it, raised to the power of exp, equals the input matrix
+    '''
+    W, V = np.linalg.eig(A) # A = V diag(W) V^{-1}
+    W = np.array([pow(x, exp) if x > 0 else 0 for x in W])
     return V @ np.diag(W) @ V.T
