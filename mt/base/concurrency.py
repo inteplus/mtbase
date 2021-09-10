4b1878de93cea2e7caca5c02fcfe93cd991f9cbf
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2021-09-10 09:49:04 +0100

    v1.15: mt.base.concurrency.asyn_work_generator() partially tested

--- mt/base/concurrency.py
+++ mt/base/concurrency.py
@@ -1,19 +1,19 @@
 '''Concurrency in dask way. Needed for streaming workloads.'''
 
 import os
-import numpy as np
-import queue as _q
-import threading as _t
-import multiprocessing as _mp
-from time import sleep
-import psutil
 import asyncio
+from time import sleep
+import multiprocessing as _mp
+import threading as _t
+import queue as _q
+import numpy as np
+import psutil
 
 from .bg_invoke import BgInvoke, BgThread, BgException
 
 
 __all__ = ['Counter', 'used_memory_too_much', 'used_cpu_too_much', 'split_works', 'ProcessParalleliser', 'WorkIterator', 'serial_work_generator',
-           'aio_work_generator', 'run_asyn_works_in_new_context', 'run_asyn_works']
+           'aio_work_generator', 'run_asyn_works_in_context', 'asyn_work_generator']
 
 
 class Counter(object):
@@ -641,8 +641,8 @@ async def aio_work_generator(func, num_work_ids, skip_null: bool = True, max_con
                     yield result
 
 
-async def run_asyn_works_in_new_context(progress_queue: _mp.Queue, func, func_args: list = [], func_kwargs: dict = {}, context_id = None, work_id_list: list = [], max_concurrency: int = 1024, asyn: bool = True, profile = None):
-    '''Invokes the same asyn function with different work ids concurrently and asynchronously, and under a new context.
+async def run_asyn_works_in_context(progress_queue: _mp.Queue, func, func_args: list = [], func_kwargs: dict = {}, context_id = None, work_id_list: list = [], max_concurrency: int = 1024, asyn: bool = True, context_vars: dict = {}):
+    '''Invokes the same asyn function with different work ids concurrently and asynchronously, in a given context.
 
     Parameters
     ----------
@@ -667,14 +667,23 @@ async def run_asyn_works_in_new_context(progress_queue: _mp.Queue, func, func_ar
         allocations. If None is given, all works in the context will be scheduled to run at once.
     asyn : bool
         whether the asyn function is to be invoked asynchronously or synchronously
-    profile : str, optional
-        one of the profiles specified in the AWS. The default is used if None is given.
+    context_vars : dict
+        dictionary of context variables with which the function runs. Variable 's3_client' must
+        exist and hold an enter-result of an async with statement invoking
+        :func:`mt.base.s3.create_s3_client`. In asynchronous mode, variable 'http_session' must
+        exist and hold an enter-result of an async with statement invoking
+        :func:`mt.base.http.create_http_session`. You can use
+        :func:`mt.base.s3.create_context_vars` to create a dictionary like this.
 
     Notes
     -----
     This function returns nothing but while it is running, the progress queue regularly receives
     messages. Each message is a tuple `(context_id, message_code, work_id, ...)`. The user should
     process the progress queue.
+
+    If KeyboardInterrupt is raised during the invocation, all remaining unscheduled tasks are
+    cancelled. This means for the case that max_concurrency is None, KeyboardInterrupt has no
+    effect.
     '''
 
     def get_done_task_result(task, work_id):
@@ -689,59 +698,66 @@ async def run_asyn_works_in_new_context(progress_queue: _mp.Queue, func, func_ar
         result = task.result()
         return (context_id, 'task_returned', work_id, result)
 
-    progress_queue.put_nowait((context_id, 'context_created'))
-
     import asyncio
-    from .s3 import create_context_vars
+    keyboard_interrupted = False
     
-    async with create_context_vars(asyn=asyn, profile=profile) as context_vars:
-        if max_concurrency is None:
-            task_map = {}
-            for work_id in work_id_list:
-                task = asyncio.ensure_future(func(work_id, *func_args, asyn=asyn, context_vars=context_vars, **func_kwargs))
-                progress_queue.put_nowait((context_id, 'task_scheduled', work_id))
-                task_map[task] = work_id
+    if max_concurrency is None:
+        task_map = {}
+        for work_id in work_id_list:
+            task = asyncio.ensure_future(func(work_id, *func_args, asyn=asyn, context_vars=context_vars, **func_kwargs))
+            progress_queue.put_nowait((context_id, 'task_scheduled', work_id))
+            task_map[task] = work_id
 
-            while len(task_map) > 0:
-                done_task_set, _ = await asyncio.wait(task_map.keys(), return_when=asyncio.FIRST_COMPLETED)
-                for task in done_task_set:
-                    work_id = task_map[task]
-                    progress_queue.put_nowait(get_done_task_result(task, work_id))
-                    del task_map[task]
+        while len(task_map) > 0:
+            done_task_set, _ = await asyncio.wait(task_map.keys(), return_when=asyncio.FIRST_COMPLETED)
 
-        else:
-            cur_pos = 0
-            cur_task_map = {}
+            for task in done_task_set:
+                work_id = task_map[task]
+                msg = get_done_task_result(task, work_id)
+                progress_queue.put_nowait(msg)
+                del task_map[task]
+                if msg[1] == 'task_raised' and isinstance(msg[3], KeyboardInterrupt):
+                    keyboard_interrupted = True
 
-            while cur_pos < len(work_id_list) or len(cur_task_map) > 0:
-                # add tasks to run concurrently
-                spare = min(max_concurrency - len(cur_task_map), len(work_id_list) - cur_pos)
-                if spare > 0:
-                    for i in range(spare):
-                        work_id = work_id_list[cur_pos + i]
+    else:
+        cur_pos = 0
+        cur_task_map = {}
+
+        while cur_pos < len(work_id_list) or len(cur_task_map) > 0:
+            # add tasks to run concurrently
+            spare = min(max_concurrency - len(cur_task_map), len(work_id_list) - cur_pos)
+            if spare > 0:
+                for i in range(spare):
+                    work_id = work_id_list[cur_pos + i]
+                    if keyboard_interrupted:
+                        progress_queue.put_nowait((context_id, 'task_cancelled', work_id))
+                    else:
                         task = asyncio.ensure_future(func(work_id, *func_args, asyn=asyn, context_vars=context_vars, **func_kwargs))
                         progress_queue.put_nowait((context_id, 'task_scheduled', work_id))
                         cur_task_map[task] = work_id
-                    cur_pos += spare
+                cur_pos += spare
 
-                # get some tasks done
-                done_task_set, _ = await asyncio.wait(cur_task_map.keys(), return_when=asyncio.FIRST_COMPLETED)
-                for task in done_task_set:
-                    work_id = cur_task_map[task]
-                    progress_queue.put_nowait(get_done_task_result(task, work_id))
-                    del cur_task_map[task]
+            # get some tasks done
+            done_task_set, _ = await asyncio.wait(cur_task_map.keys(), return_when=asyncio.FIRST_COMPLETED)
+            for task in done_task_set:
+                work_id = cur_task_map[task]
+                msg = get_done_task_result(task, work_id)
+                progress_queue.put_nowait(msg)
+                del cur_task_map[task]
+                if msg[1] == 'task_raised' and isinstance(msg[3], KeyboardInterrupt):
+                    keyboard_interrupted = True
 
-    progress_queue.put_nowait((context_id, 'context_destroyed'))
 
+async def asyn_work_generator(func, func_args: list = [], func_kwargs: dict = {}, num_processes = None, num_works: int = 0, max_concurrency: int = 1024, profile = None, debug_logger = None, progress_queue = None):
+    '''An asyn generator that does a large number of works concurrently and yields the work results.
 
-def run_asyn_works(progress_queue: _mp.Queue, func, func_args: list = [], func_kwargs: dict = {}, num_processes = None, num_works: int = 0, max_concurrency: int = 1024, profile = None):
-    '''Splits the list of work ids into blocks and invokes :func:`run_asyn_works_in_new_context` to run each block in a new context in a separate process.
+    Internally, it splits the list of work ids into blocks and invokes
+    :func:`run_asyn_works_in_context` to run each block in a new context in a separate process. It
+    uses a multiprocessing queue to facilitate the communications between the processes. Every time
+    a task is done, either returning a result or raising an exception, it yields that information.
 
     Parameters
     ----------
-    progress_queue: multiprocessing.Queue
-        a shared queue so that the main process can observe the progress inside every context. See
-        notes below.
     func : function
         an asyn function that may return something and may raise an Exception. The function must
         have the first argument being the work id, and must accept keyword argument 'asyn' and
@@ -761,46 +777,61 @@ def run_asyn_works(progress_queue: _mp.Queue, func, func_args: list = [], func_k
         once.
     profile : str, optional
         one of the profiles specified in the AWS. The default is used if None is given.
-
-    Returns
-    -------
-    process_list : list
-        list of processes that have been started to run the function
+    debug_logger : logging.Logger or equivalent
+        logger for debugging purposes, if needed
+    progress_queue: multiprocessing.Queue
+        a shared queue so that the main process can observe the progress inside the context. If not
+        provided, one will be created internally.
 
     Notes
     -----
-    This function returns as soon as it launches all the processes. While it is running, the
-    progress queue regularly receives messages. Each message is a tuple
-    `(context_id, message_code, work_id, ...)`. The user should process the progress queue.
-
-    The context ids are zero-based. The work ids are zero-based. The number of contexts is equal to
-    the number of processes.
+    The context ids are zero-based. The number of contexts is equal to the number of processes.
+    The work ids are task ids and are zero-based. The messages related to when a task is done are
+    yielded.
     '''
 
     if num_works <= 0:
         return
 
+    # for communicating between processes
+    queue = _mp.Queue() if progress_queue is None else progress_queue
+
     num_buckets = _mp.cpu_count() if num_processes is None else num_processes
     work_id_list_list = split_works(num_works, num_buckets)
 
+
     def worker_process(progress_queue: _mp.Queue, func, func_args: list = [], func_kwargs: dict = {}, context_id = None, work_id_list: list = [], max_concurrency: int = 1024, profile = None):
         import asyncio
-        asyncio.run(run_asyn_works_in_new_context(
-            progress_queue, func,
-            func_args=func_args,
-            func_kwargs=func_kwargs,
-            context_id=context_id,
-            work_id_list=work_id_list,
-            max_concurrency=max_concurrency,
-            asyn=True,
-            profile=profile,
-        ))
+        from .s3 import create_context_vars
 
+        async def asyn_func():
+            async with create_context_vars(asyn=True, profile=profile) as context_vars:
+                content = await run_asyn_works_in_context(
+                    progress_queue, func,
+                    func_args=func_args,
+                    func_kwargs=func_kwargs,
+                    context_id=context_id,
+                    work_id_list=work_id_list,
+                    max_concurrency=max_concurrency,
+                    asyn=True,
+                    context_vars=context_vars,
+                )
+            return content
+
+        progress_queue.put_nowait((context_id, 'context_created'))
+        try:
+            asyncio.run(asyn_func())
+        except KeyboardInterrupt as e: # asyncio sucks
+            from .traceback import extract_stack_compact
+            progress_queue.put_nowait((context_id, 'context_raised', e, extract_stack_compact()))
+        progress_queue.put_nowait((context_id, 'context_destroyed'))
+
+    # launch the concurrency suite
     process_list = []
     for context_id in range(num_buckets):
         p = _mp.Process(
             target=worker_process,
-            args=(progress_queue, func),
+            args=(queue, func),
             kwargs={
                 'func_args': func_args,
                 'func_kwargs': func_kwargs,
@@ -812,4 +843,38 @@ def run_asyn_works(progress_queue: _mp.Queue, func, func_args: list = [], func_k
         p.start()
         process_list.append(p)
 
-    return process_list
+    # wait for every task to be done
+    num_running_buckets = num_buckets
+    wait_cnt = 0
+    keyboard_interrupted = False
+    while (num_running_buckets > 0) and wait_cnt < 300:
+        try:
+            msg = queue.get_nowait()
+            wait_cnt = 0
+            if debug_logger:
+                debug_logger.debug("asyn_work_generator: {}".format(msg))
+            if msg[1] == 'context_destroyed':
+                num_running_buckets -= 1
+            elif msg[1] in ('task_returned', 'task_cancelled', 'task_raised'):
+                yield msg[1:]
+        except _q.Empty:
+            try:
+                await asyncio.sleep(0.1)
+            except KeyboardInterrupt:
+                if debug_logger:
+                    debug_logger.warn("Keyboard interrupted. Will reraise when the time is right.")
+                keyboard_interrupted = True
+            wait_cnt += 1
+
+    # clean up
+    if wait_cnt < 300: # healthy
+        for p in process_list:
+            p.join()
+    else: # something wrong
+        if debug_logger:
+            debug_logger.debug("asyn_work_generator: 30s timeout reached.")
+        for p in process_list:
+            p.terminate() # ouch!
+
+    if keyboard_interrupted:
+        raise KeyboardInterrupt("Keyboard interrupted while asyn_work_generator() is running.")
