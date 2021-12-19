e820b3affb683a34faad97d32733fbfba30ae9be
Minh-Tri Pham <pmtri80@gmail.com>
2021-12-19 09:07:27 +0000

    bug fix

--- mt/base/concurrency/beehive.py
+++ mt/base/concurrency/beehive.py
@@ -713,9 +713,9 @@ class QueenBee(WorkerBee):
             if not process.is_alive():
                 continue
             process.join(10) # give maximum 10 seconds for each process to terminate
-            if process.exit_code is not None:
+            if process.exitcode is not None:
                 if self.logger:
-                    self.logger.warn("Subprocess {} has terminated with exit code {}.".format(process.pid, process.exit_code))
+                    self.logger.warn("Subprocess {} has terminated with exit code {}.".format(process.pid, process.exitcode))
 
 
     # ----- private -----
