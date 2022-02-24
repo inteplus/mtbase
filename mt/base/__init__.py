c753f105a56c56e62db37890f069fdfa53661525
Minh-Tri Pham <pmtri80@gmail.com>
2022-02-24 09:19:36 +0000

    v2.12: introducing variable prefix naming convention

--- mt/base/__init__.py
+++ mt/base/__init__.py
@@ -1,3 +1,20 @@
+'''Minh-Tri Pham's base package for Python.
+
+
+# Variable prefix naming convention
+
+On 2022/02/24, I introduced a prefix naming convention for variables. All variable names have been
+gradually updated since then. The convention is described in the following paragraphs.
+
+Variables can have a prefix in the form of a letter followed by an underscore. 'l_' stands for a
+list of things. 't_' stands for a tuple of things. 'a_' stands for a :class:`numpy.ndarray` which
+is an array of things. 'b_' stands for a :class:`tensorflow.Tensor` which is a batch of things.
+
+Nested collections can be prefixed with multiple letters followed by an underscore, using the same
+convention as above. For example, 'll_' stands for a list of lists of things.
+
+'''
+
 from .with_utils import dummy_scope, join_scopes
 from .logging import make_logger, logger, init as _log_init
 from .deprecated import deprecated_func
