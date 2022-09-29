b05b050a23942ded96976b1115fad32d48d6a3da
Minh-Tri <minhtri.pham@winnowsolutions.com>
2022-09-29 12:21:16 +0100

    bug fix

--- mt/np/__init__.py
+++ mt/np/__init__.py
@@ -13,12 +13,10 @@ from .image import *
 
 ndarray = _np.ndarray
 import numpy.core as core
-from numpy.core import *
+from numpy.core import round
 import numpy.lib as lib
-from numpy.lib import *
 import numpy.polynomial as polynomial
 import numpy.linalg as linag
 import numpy.fft as fft
 import numpy.matlib as matlib
-from numpy.matlib import *
 import numpy.random as random
