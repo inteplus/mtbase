bc83e44cf54ae5951b670c1cf412effaae15a0ec
Minh-Tri Pham <pmtri80@gmail.com>
2022-12-31 10:13:17 +0000

    move mt.base.traceback -> mt.traceback

---
+++ mt/traceback/__init__.py
@@ -0,0 +1,25 @@
+"""Traceback extra
+
+Please see :module:`traceback` for more details.
+"""
+
+import traceback as _tb
+from traceback import *
+
+
+__all__ = ["format_exc_info", "extract_stack_compact"]
+
+
+def format_exc_info(exc_type, exc_value, exc_traceback):
+    """Formats (exception type, exception value, traceback) into multiple lines."""
+    statements = _tb.format_exception(exc_type, exc_value, exc_traceback)
+    statements = "".join(statements)
+    return statements.split("\n")
+
+
+def extract_stack_compact():
+    """Returns the current callstack in a compact format."""
+    lines = _tb.format_list(_tb.extract_stack())
+    lines = "".join(lines).split("\n")
+    lines = [line for line in lines if "frozen importlib" not in line]
+    return lines
