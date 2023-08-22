12029f056a47956635c9f778308e022fb9eb6227
Minh-Tri Pham <pmtri80@gmail.com>
2023-08-22 16:13:21 +0100

    v4.8: make pyximportcpp compatible with Cython 3.0

--- make_version.sh
+++ make_version.sh
@@ -14,7 +14,7 @@
 # ```
 
 MAJOR_VERSION=4
-MINOR_VERSION=7
+MINOR_VERSION=8
 
 VERSION_YEAR=`date -u +'%Y'`
 VERSION_MONTH=`date -u +'%m'`
--- mt/base/pyximportcpp.py
+++ mt/base/pyximportcpp.py
@@ -2,12 +2,21 @@ import numpy as np
 import pyximport
 from pyximport import install
 
-old_get_distutils_extension = pyximport.pyximport.get_distutils_extension
+try:
+    from pyximport import _pyximport3 as _pi
+except ImportError:
+    from pyximport import pyximport as _pi
+
+old_get_distutils_extension = _pi.get_distutils_extension
+
 
 def new_get_distutils_extension(modname, pyxfilename, language_level=None):
-    extension_mod, setup_args = old_get_distutils_extension(modname, pyxfilename, language_level)
-    extension_mod.language='c++'
+    extension_mod, setup_args = old_get_distutils_extension(
+        modname, pyxfilename, language_level
+    )
+    extension_mod.language = "c++"
     extension_mod.include_dirs = [np.get_include()]
-    return extension_mod,setup_args
+    return extension_mod, setup_args
 
-pyximport.pyximport.get_distutils_extension = new_get_distutils_extension
+
+_pi.get_distutils_extension = new_get_distutils_extension
