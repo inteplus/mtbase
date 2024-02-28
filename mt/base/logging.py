6bc7983461718f926fe8ff6a1be41fca9c1e8209
Minh-Tri Pham <pmtri80@gmail.com>
2024-02-28 04:53:49 +0000

    v4.26.2: removed a redundant module

--- mt/base/logging.py
+++
@@ -1,6 +0,0 @@
-from logging import *
-from mt.logging import *
-from mt.logg import logger
-
-# MT-TODO: make the module deprecated starting from v4.0.
-logger.warn_module_move("mt.base.logging", "mt.logg")
