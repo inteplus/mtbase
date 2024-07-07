dd17b8b1e80c7e4153c29c9ee8c21dec96c374f9
Minh-Tri Pham <pmtri80@gmail.com>
2024-07-07 15:57:17 +0100

    updated version.py to 4.29.3

--- mt/base/datatype.py
+++ mt/base/datatype.py
@@ -1,6 +1,7 @@
 """Submodules dealing with datatypes."""
 
 import sys
+import traceback
 
 
 def is_ndarray(a) -> bool:
@@ -42,12 +43,11 @@ def is_h5group(a) -> bool:
 class LogicError(RuntimeError):
     """An error in the logic, defined by a message and a debugging dictionary.
 
-    As of 2024/07/07, the user can optionally provide the error that caused this error and
-    optionally the callstack (in list of lines) of the causing error.
+    As of 2024/07/07, the user can optionally provide the error that caused this error.
     """
 
-    def __init__(self, msg, debug={}, causing_error=None, causing_callstack=[]):
-        super().__init__(msg, debug, causing_error, causing_callstack)
+    def __init__(self, msg, debug={}, causing_error=None):
+        super().__init__(msg, debug, causing_error)
 
     def __str__(self):
         l_lines = []
@@ -56,10 +56,12 @@ class LogicError(RuntimeError):
         if causing_error:
             msg = f"With the following {type(causing_error).__name__}:"
             l_lines.append(msg)
-            causing_callstack = self.args[3]
-            if causing_callstack:
-                l_lines.append("  Callstack:")
-                for line in causing_callstack:
+            causing_stacktrace = causing_error.__traceback__
+            if causing_stacktrace:
+                causing_stacktrace = traceback.format_tb(causing_stacktrace)
+                causing_stacktrace = "".join(causing_stacktrace).split("\n")
+                l_lines.append("  Stack trace:")
+                for line in causing_stacktrace:
                     l_lines.append("  " + line)
             for line in str(causing_error).split("\n"):
                 l_lines.append("  " + line)
