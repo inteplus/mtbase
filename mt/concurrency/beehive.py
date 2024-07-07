3a77d21699e1b85edb5f7e4fee88d7fccad4e99f
Minh-Tri Pham <pmtri80@gmail.com>
2024-07-07 19:52:23 +0100

    updated version.py to 4.30.8

--- mt/concurrency/beehive.py
+++ mt/concurrency/beehive.py
@@ -448,6 +448,7 @@ class Bee:
                     line = linecache.getline(filename, lineno, f.f_globals)
                     extracted_list.append((filename, lineno, name, line))
                 stacktrace = extracted_list
+                task.exception().__traceback__ = stacktrace
                 stacktrace = [str(stacktrace)]
             else:
                 import io
